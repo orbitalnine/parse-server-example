f130796380a21993118c59d9cd600a824fadcd2e
Aaron Lake <orbitalninegames@gmail.com>
2020-12-09 15:18:30 -0800

    Removing ip address from list

--- cloud/main.js
+++ cloud/main.js
@@ -5,7 +5,7 @@ Parse.Cloud.define('hello', function(req, res)
 
 Parse.Cloud.define('validate', async(request) =>
 {
-	var bannedIPs = ["109.12.81.168", "70.36.60.80"];
+	var bannedIPs = ["109.12.81.168"];
 	
 	var clientIP = request.headers['x-forwarded-for'];
 	if (bannedIPs.indexOf(clientIP) >= 0)
