b0a28b749221dee5d20c81e3d9b31772f4bb6285
Aaron Lake <orbitalninegames@gmail.com>
2020-12-08 22:44:47 -0800

    no message

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  logLevel: "verbose"
 });
 
 /*console.log("LOG_LEVEL = " + (process.env.LOG_LEVEL || "INFO"));
