3c7d572bed6c943a6b6f011cc97f1b8d1d03297c
Aaron Lake <orbitalninegames@gmail.com>
2020-12-09 12:46:53 -0800

    Adding a validate function to check if the user is banned

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,13 @@
-Parse.Cloud.define('hello', function(req, res) {
+Parse.Cloud.define('hello', function(req, res) 
+{
   return 'Hello World!';
+});
+
+Parse.Cloud.define('validate', async(request) =>
+{
+	var clientIP = request.headers['x-forwarded-for'];
+	if (clientIP == "109.12.81.168")
+		return false;
+	else
+		return true;
 });
