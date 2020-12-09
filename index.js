799aac6c79dae75c10e8a3d30efb73f8fc9dee65
Aaron Lake <orbitalninegames@gmail.com>
2020-12-08 22:47:13 -0800

    no message

--- index.js
+++ index.js
@@ -11,19 +11,7 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  logLevel: "verbose"
-});
-
-/*console.log("LOG_LEVEL = " + (process.env.LOG_LEVEL || "INFO"));
+console.log("LOG_LEVEL = " + (process.env.LOG_LEVEL || "info"));
 
 var serverOptions = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
@@ -34,10 +22,10 @@ var serverOptions = {
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
-  logLevel: process.env.LOG_LEVEL || "INFO"
+  logLevel: process.env.LOG_LEVEL || "info"
 };
 
-if ((process.env.LOG_LEVEL && process.env.LOG_LEVEL == "VERBOSE") ||
+if ((process.env.LOG_LEVEL && process.env.LOG_LEVEL == "verbose") ||
     (process.env.VERBOSE))
 {
 	serverOptions["verbose"] = 1;
@@ -46,7 +34,7 @@ if ((process.env.LOG_LEVEL && process.env.LOG_LEVEL == "VERBOSE") ||
 console.log("Server Options: ");
 console.log(JSON.stringify(serverOptions, null, 2));
 
-var api = new ParseServer(serverOptions);*/
+var api = new ParseServer(serverOptions);
 
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
