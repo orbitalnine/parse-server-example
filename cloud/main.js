efdfc0196d8c0f4fcded3a0a02782ee7e412369a
Aaron Lake <orbitalninegames@gmail.com>
2020-12-09 15:12:57 -0800

    Improving the validate ip code to check for the current ip in a list of banned ips

--- cloud/main.js
+++ cloud/main.js
@@ -5,9 +5,10 @@ Parse.Cloud.define('hello', function(req, res)
 
 Parse.Cloud.define('validate', async(request) =>
 {
-	console.log("Got here");
+	var bannedIPs = ["109.12.81.168", "70.36.60.80"];
+	
 	var clientIP = request.headers['x-forwarded-for'];
-	if (clientIP == "109.12.81.168")
+	if (bannedIPs.indexOf(clientIP) >= 0)
 		return false;
 	else
 		return true;
