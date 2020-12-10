a9407fec3cd641a250a068e7c2b2d33a036d7724
Aaron Lake <orbitalninegames@gmail.com>
2020-12-09 21:56:59 -0800

    Adding in validateUser method to log each user that connects

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,6 @@
+
+var m_bannedIPs = ["109.12.81.168", "70.36.60.80"];
+
 Parse.Cloud.define('hello', function(req, res) 
 {
   return 'Hello World!';
@@ -5,11 +8,40 @@ Parse.Cloud.define('hello', function(req, res)
 
 Parse.Cloud.define('validate', async(request) =>
 {
-	var bannedIPs = ["109.12.81.168"];
-	
 	var clientIP = request.headers['x-forwarded-for'];
-	if (bannedIPs.indexOf(clientIP) >= 0)
+	if (m_bannedIPs.indexOf(clientIP) >= 0)
 		return false;
 	else
 		return true;
+});
+
+Parse.Cloud.define('validateUser', async(request) =>
+{
+	var clientIP = request.headers['x-forwarded-for'];
+	var currentDate = new Date();
+	var banned = (m_bannedIPs.indexOf(clientIP) >= 0);
+	
+	const User = Parse.Object.extend("User");
+	const user = new User();
+	user.set("username", request.params.username);
+	user.set("userid", request.params.userid);
+	user.set("lastlogin", currentDate);
+	user.set("banned", banned);
+	user.save();
+	
+	if (banned)
+		return false;
+	else
+		return true;
+},{
+	fields : {
+		username : {
+			required: true,
+			type: string
+		},
+		userid : {
+			required: true,
+			type: string
+		}
+	}
 });
