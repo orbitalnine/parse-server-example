78ddec699944692941672c8662023fbf3ee5add0
Aaron Lake <orbitalninegames@gmail.com>
2020-12-10 23:07:18 -0800

    Removing batch file

--- start.bat
+++
@@ -1,8 +0,0 @@
-set LOG_LEVEL=info
-set APP_ID=DjUh32Rggp8TnUfQA3MlgMCVIrCE8eDcmFYPscOj
-set MASTERKEY=LTNWPegz5WIoUm0koI1VT3jxM3k9yiOxNVIxKfVS
-set MONGODB_URI=mongodb+srv://heroku_961fv3xk:tup5cognlopecintrpd1ipq28e@cluster-961fv3xk.ykodl.mongodb.net/heroku_961fv3xk?retryWrites=true&w=majority
-set SERVER_URL=http://127.0.0.1:1337/parse/
-set PARSE_MOUNT=/parse
-
-npm start
